36e1fd04e5066c6c3a5545c214b4b8f87f743f3e
Emre <emre.erol@hotmail.com>
2024-09-04 16:47:20 +0300

    verilerin birleştirilmesi

--- kategorik.py
+++ kategorik.py
@@ -13,6 +13,7 @@ from sklearn import preprocessing
 veriler = pd.read_csv('eksikveriler.csv')
 
 ulke = veriler.iloc[:,0:1].values
+Yas=veriler.iloc[:,1:4].values
 print(ulke)
 
 le = preprocessing.LabelEncoder()
@@ -21,3 +22,16 @@ print(ulke)
 ohe=preprocessing.OneHotEncoder()
 ulke = ohe.fit_transform(ulke).toarray()
 print(ulke)
+
+print(list(range(22)))
+sonuc = pd.DataFrame(data = ulke, index=range(22),columns=['fr','tr','us'])
+print(sonuc)
+
+sonuc2 = pd.DataFrame(data = Yas,index=range(22),columns=['boy','kilo','yas'])
+print(sonuc2)
+
+cinsiyet = veriler.iloc[:,-1].values
+print(cinsiyet)
+
+sonuc3=pd.DataFrame(data=cinsiyet,index=range(22),columns=['cinsiyet'])
+print(sonuc)
